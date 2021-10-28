947191beebe69be912962673110e9a6db47cc420
Subrat Shrestha <subrat.shrestha3@gmail.com>
2021-10-28 14:41:45 +1100

    range query, point update

---
+++ range_query_update.cpp
@@ -0,0 +1,8 @@
+/*
+    Segment tree to perform range updates and range queries in O(logN) time.
+    Ex.
+    Given an array a[N], initially all zeros, support Q operations, each being
+    one of the following forms:
+    Update: U l r v. Perform a[l,..,r) += v.
+    Query: Q l r. Output a[l] + ... + a[r-1].
+*/
