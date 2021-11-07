796f694ab91520a0dd663d440d06a0a535539101
Subrat Shrestha <subrat.shrestha3@gmail.com>
2021-11-07 11:40:16 +1100

    added negative cycle detection for bellman ford

--- bellman.cpp
+++ bellman.cpp
@@ -20,31 +20,55 @@ void bellman() {
     int s = 0;
     dist[s] = 0;
     vector<int> prev(N, -1);
+    int x;
 
     // relax all edges at most N-1 times.
     for (int i = 0; i < N - 1; i++) {
+        x = -1;
         bool hasChanges = false;
 
         for (int j = 0; j < M; j++) {
             if (dist[edges[j].u] < INF) {
-                dist[edges[j].v] = min(dist[edges[j].v], dist[edges[j].u] + edges[j].d);
-                prev[edges[j].v] = edges[j].u;
-                hasChanges = true;
+                if (dist[edges[j].v] < dist[edges[j].u] + edges[j].d) {
+                    dist[edges[j].v] = max(-INF, dist[edges[j].u] + edges[j].d);
+                    prev[edges[j].v] = edges[j].u;
+                    hasChanges = true;
+                    x = edges[j].v;
+                }
             }
         }
 
         if (!hasChanges) break;
     }
 
-    // getting path.
-    int dest = N-1;
-    if (dist[dest] == INF)
-        cout << "no path from source = " << s << " to destination = " << dest << '\n';
-    else {
-        int cur = dest;
-        while (cur != -1) {
+    if (x == -1) {
+        cout << "no negative cycle\n";
+
+        // getting path.
+        int dest = N-1;
+        if (dist[dest] == INF)
+            cout << "no path from source = " << s << " to destination = " << dest << '\n';
+        else {
+            int cur = dest;
+            while (cur != -1) {
+                path.push_back(cur);
+                cur = prev[cur];
+            }
+
+            reverse(path.begin(), path.end());
+        }
+    } else {
+        // get negative cycle path.
+        cout << "negative cycle\n";
+        int y = x;
+        for (int i = 0; i < N; i++) {
+            y = prev[y];
+        }
+
+        for (int cur = y; ; cur = prev[cur]) {
             path.push_back(cur);
-            cur = prev[cur];
+            if (cur == y && path.size() > 1)
+                break;
         }
         reverse(path.begin(), path.end());
     }
