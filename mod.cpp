6062a1f627d7175916b17d325f1971c47e279beb
Subrat Shrestha <subrat.shrestha3@gmail.com>
2021-11-24 23:08:40 +1100

    added header for safe mod

--- mod.cpp
+++ mod.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// safe mod function for subtractions, deals with negatives.
 long long safe_mod(long long a, long long b, int mod) {
     return (((a - b) % mod) + mod) % mod;
 }
